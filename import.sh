699aaad093ce284f5d520fbc1ee417e605bad310
monpaf <227411594+monpaf@users.noreply.github.com>
2026-07-10 09:00:45 +0200

    chore: add import script

---
+++ import.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+UPSTREAM_URL="https://github.com/The-Red-Wizard/TheRedWizard.git"
+ADDON="plugin.video.redlight"
+XML_FILE="$ADDON/addon.xml"
+
+if ! git remote | grep -q "^upstream$"; then
+    git remote add upstream "$UPSTREAM_URL"
+fi
+
+git fetch upstream
+git checkout upstream/main -- "$ADDON"
+
+VERSION=$(sed -n 's/.*[^0-9]\([0-9]\+\.[0-9]\+\.[0-9]\+\)">.*/\1/p' "$XML_FILE" | head -n 1)
+
+git add "$ADDON"
+git commit -m "chore: import v$VERSION"
